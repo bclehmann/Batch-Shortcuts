9bd335af6c0a32e39f5c68db4492dec697a2f5a2
Benjamin Lehmann <bclehmann@yahoo.ca>
2019-09-14 14:26:28 -0600

    Full C# passwordgen code

--- passwordGen.bat
+++ passwordGen.bat
@@ -1,40 +1,44 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
-SET /A "length=32"
-SET /A "bitspace=65536"
+REM SET /A "length=32"
+REM SET /A "bitspace=65536"
 
-if NOT [%1]==[] set /A "length=%1"
+REM if NOT [%1]==[] set /A "length=%1"
 
-::echo %length%
+REM ::echo %length%
 
-set "charset=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^*`~,.-/"
+REM set "charset=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^*`~,.-/"
 
-set "out="
+REM set "out="
 
-set /A "i=0"
+REM set /A "i=0"
 
-:loop
-if NOT %i% LSS %length% goto end
-	:: Changes range to be [0-76], the length of the charset
-	set RET=cont
-	goto :RandomGen
-	:cont
-		set /A "rand=%rand% * 76 / %bitspace%"
-		set out=%out%!charset:~%rand%,1!
-
-
-	set /A i+=1
-goto loop
+REM :loop
+REM if NOT %i% LSS %length% goto end
+	REM :: Changes range to be [0-76], the length of the charset
+	REM set RET=cont
+	REM goto :RandomGen
+	REM :cont
+		REM set /A "rand=%rand% * 76 / %bitspace%"
+		REM set out=%out%!charset:~%rand%,1!
 
 
-:end
-	echo %out%
-	goto :eof
+	REM set /A i+=1
+REM goto loop
 
-:RandomGen
-	dotnet "%~dp0/RNG.dll" bytes=2 > tmpFile 
-	set /p temp= < tmpFile 
-	del tmpFile 
-	set /A "rand=%temp%"
-	::echo %rand%
-	goto %RET%
+
+REM :end
+	REM echo %out%
+	REM goto :eof
+
+REM :RandomGen
+	REM dotnet "%~dp0/RNG.dll" bytes=2 > tmpFile 
+	REM set /p temp= < tmpFile 
+	REM del tmpFile 
+	REM set /A "rand=%temp%"
+	REM ::echo %rand%
+	REM goto %RET%
+	
+dotnet "%~dp0/NetCorePasswordgen.dll" %* > tmpFile 
+type tmpFile
+del tmpFile
