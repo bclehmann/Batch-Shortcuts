63dd66e59e1cea4479a9a72b371f7d3aa826ab8d
Benjamin Lehmann <bclehmann@yahoo.ca>
2019-09-13 19:38:32 -0600

    Password Generator

---
+++ passwordGen.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+SET /A "length=32"
+
+
+
+if NOT [%1]==[] set /A "length=%1"
+
+::echo %length%
+
+set "charset=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^*`~,.-/"
+
+set "out="
+
+set /A "i=0"
+
+:loop
+if NOT %i% LSS %length% goto end
+	:: Changes range to be [0-76], the length of the charset
+	set /A "rand=%RANDOM% * 76 / 32768"
+	set out=%out%!charset:~%rand%,1!
+
+
+	set /A i+=1
+goto loop
+
+
+:end
+	echo %out%
